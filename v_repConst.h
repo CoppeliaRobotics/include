20bcf7a83777f5de230d091f0f5c903ce485b6ed
Coppelia <mfreese@gmx.ch>
2018-12-12 11:32:05 +0100

    rev 14

--- v_repConst.h
+++ v_repConst.h
@@ -4,8 +4,8 @@
 #define VREP_PROGRAM_VERSION_NB 30500
 #define VREP_PROGRAM_VERSION "3.5.0."
 
-#define VREP_PROGRAM_REVISION_NB 13
-#define VREP_PROGRAM_REVISION "(rev. 13)"
+#define VREP_PROGRAM_REVISION_NB 14
+#define VREP_PROGRAM_REVISION "(rev. 14)"
 
 /* Scene object types. Values are serialized */
 enum {
