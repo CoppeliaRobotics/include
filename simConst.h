eb63a53b34f88b84b18a517b1b3eb48b4dcab0b6
Coppelia <mfreese@gmx.ch>
2022-09-02 14:37:42 +0200

    sim_stringparam_resourcesdir

--- simConst.h
+++ simConst.h
@@ -897,6 +897,7 @@ enum { /* String parameters: */
     sim_stringparam_mujocodir, /* can only be read */
     sim_stringparam_usersettingsdir, /* can only be read */
     sim_stringparam_systemdir, /* can only be read */
+    sim_stringparam_resourcesdir, /* can only be read */
 };
 
 enum { /* Array parameters: */
