e4463ff7df1367b38b079ee45d9fb39b445d593b
Federico Ferri <federico.ferri.it@gmail.com>
2023-03-29 15:04:37 +0200

    typo

--- simStubsGen/generate_lua_typechecker.py
+++ simStubsGen/generate_lua_typechecker.py
@@ -32,7 +32,7 @@ function sim{plugin.name}.__addTypeCheck()
         f.write(f"""
     wrapFunc('{cmd.name}',function(origFunc)
         return function(...)
-            {paramsList and 'local ' or ''}{paramList}{paramList and '=' or ''}checkargsEx(
+            {paramList and 'local ' or ''}{paramList}{paramList and '=' or ''}checkargsEx(
                 {{level=1}},
                 {{
 """)
