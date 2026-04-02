ee123ded65600caa05130065e842dcd801a78dad
Federico Ferri <federico.ferri.it@gmail.com>
2026-04-02 16:14:34 +0200

    add <namespace> tags

--- tools/extract_objects_properties.lua
+++ tools/extract_objects_properties.lua
@@ -4,7 +4,7 @@ function sysCall_init()
     cbor = require 'simCBOR'
     lfsx = require 'lfsx'
 
-    infos = {}
+    classInfo = {}
     superclass = {sceneObject = 'object'}
     flags = {'readable', 'writable', 'removable', 'silent', 'deprecated', 'constant'}
     typemap = table.invert(table.filter(sim, {matchKeyPrefix = 'propertytype_', stripKeyPrefix = true}))
@@ -38,13 +38,16 @@ function sysCall_init()
         local objectType = h.objectType
         local objectMetaInfo = json.decode(h.objectMetaInfo)
         superclass[objectType] = objectMetaInfo.superclass
+        local info
         local ok, err = pcall(function()
             info = sim.getPropertiesInfos(h, {excludeFlags = 0})
         end)
         if not ok then
             error(string.format('target %d: %s', h.handle, err))
         end
-        infos[objectType] = infos[objectType] or {}
+        classInfo[objectType] = classInfo[objectType] or {properties = {}}
+        --FIXME: namespace info is not reported in the correct class (typically: sceneObject) but in its subclasses
+        --classInfo[objectType].namespaces = objectMetaInfo.namespaces
         for pname, pinfo in pairs(info) do
             local pclass = pinfo.class or objectType
             local fullk = pclass .. '.' .. pname
@@ -56,54 +59,91 @@ function sysCall_init()
             if string.startswith(pname, 'signal.') then goto continue end
             if string.startswith(pname, 'refs.') then goto continue end
             if string.startswith(pname, 'origRefs.') then goto continue end
-            infos[pclass] = infos[pclass] or {}
-            if infos[pclass][pname] then
-                if not table.eq(pinfo, infos[pclass][pname]) then
+            classInfo[pclass] = classInfo[pclass] or {properties = {}}
+            if classInfo[pclass].properties[pname] then
+                if not table.eq(pinfo, classInfo[pclass].properties[pname]) then
                     printf('inconsistent property info for %s.%s', pclass, pname)
-                    print(infos[pclass][pname])
+                    print(classInfo[pclass].properties[pname])
                     print(pinfo)
                     foundInconsistentFlags = true
                 end
             else
-                infos[pclass][pname] = pinfo
+                classInfo[pclass].properties[pname] = pinfo
             end
             ::continue::
         end
     end
     assert(not foundInconsistentFlags, 'found inconsistent flags')
 
+    --FIXME: namespace info is not reported in the correct class (typically: sceneObject) but in its subclasses
+    --       namespaces defined manually here:
+    classInfo.app.namespaces = {
+        customData = {},
+        signal = {},
+        namedParam = {},
+    }
+    classInfo.scene.namespaces = {
+        customData = {},
+        signal = {},
+    }
+    classInfo.sceneObject.namespaces = {
+        customData = {},
+        signal = {},
+        refs = {newPropertyForcedType = 'handlearray'},
+        origRefs = {newPropertyForcedType = 'handlearray'},
+    }
+
     -- XXX: drawing objects currently do not have any exposed property
-    infos.drawingObject.cyclic = {type = 'bool'}
-    infos.drawingObject.maxCnt = {type = 'int'}
-    infos.drawingObject.overlay = {type = 'bool'}
-    infos.drawingObject.parentUid = {type = 'long'}
-    infos.drawingObject.size = {type = 'float'}
-    infos.drawingObject.type = {type = 'string'}
-    infos.drawingObject.points = {type = 'floatarray'}
-    infos.drawingObject.quaternions = {type = 'floatarray'}
-    infos.drawingObject.colors = {type = 'floatarray'}
-    infos.drawingObject.appendPoints = {type = 'floatarray'}
-    infos.drawingObject.appendQuaternions = {type = 'floatarray'}
-    infos.drawingObject.appendColors = {type = 'floatarray'}
+    classInfo.drawingObject.properties.cyclic = {type = 'bool'}
+    classInfo.drawingObject.properties.maxCnt = {type = 'int'}
+    classInfo.drawingObject.properties.overlay = {type = 'bool'}
+    classInfo.drawingObject.properties.parentUid = {type = 'long'}
+    classInfo.drawingObject.properties.size = {type = 'float'}
+    classInfo.drawingObject.properties.type = {type = 'string'}
+    classInfo.drawingObject.properties.points = {type = 'floatarray'}
+    classInfo.drawingObject.properties.quaternions = {type = 'floatarray'}
+    classInfo.drawingObject.properties.colors = {type = 'floatarray'}
+    classInfo.drawingObject.properties.appendPoints = {type = 'floatarray'}
+    classInfo.drawingObject.properties.appendQuaternions = {type = 'floatarray'}
+    classInfo.drawingObject.properties.appendColors = {type = 'floatarray'}
 
     -- XXX: temporary
-    infos.joint.dynCtrlMode.enum = 'jointDynCtrlMode'
-    infos.scene.simulationState.enum = 'simulationState'
+    classInfo.joint.properties.dynCtrlMode.enum = 'jointDynCtrlMode'
+    classInfo.scene.properties.simulationState.enum = 'simulationState'
 
-    outputFile = {xml = sim.app.namedParam.output_xml}
-    output = {}
-    output.xml = {tag = 'object-classes', attrs = {}, children = {}}
-    clss = table.keys(infos)
-    table.sort(clss)
-    for _, cls in ipairs(clss) do
-        clsinfo = infos[cls]
-        local clsnode = {tag = 'object-class', attrs = {name = cls, superclass = superclass[cls]}, children = {}}
-        table.insert(output.xml.children, clsnode)
-        ks = table.keys(clsinfo)
-        table.sort(ks)
-        for _, k in ipairs(ks) do
-            info = clsinfo[k]
-            local propnode = {tag = 'property', attrs = {name = k, type = info.type}, children = {}}
+    outputFileXML = sim.app.namedParam.output_xml
+
+    classesNode = {
+        tag = 'object-classes',
+        attrs = {},
+        children = {},
+    }
+
+    classNames = table.keys(classInfo)
+    table.sort(classNames)
+
+    for _, className in ipairs(classNames) do
+        local classNode = {
+            tag = 'object-class',
+            attrs = {
+                name = className,
+                superclass = superclass[className],
+            },
+            children = {},
+        }
+        table.insert(classesNode.children, classNode)
+        local propertiesNames = table.keys(classInfo[className].properties)
+        table.sort(propertiesNames)
+        for _, propertyName in ipairs(propertiesNames) do
+            local pinfo = classInfo[className].properties[propertyName]
+            local propertyNode = {
+                tag = 'property',
+                attrs = {
+                    name = propertyName,
+                    type = pinfo.type,
+                },
+                children = {},
+            }
             flgs = {}
             local flgs_def = {
                 readable = true,
@@ -113,35 +153,63 @@ function sysCall_init()
                 silent = false,
                 constant = false,
             }
-            for flg, v in pairs(info.flags or {}) do
+            for flg, v in pairs(pinfo.flags or {}) do
                 if v ~= flgs_def[flg] then
                     table.insert(flgs, '"' .. flg .. '": ' .. (v and 'true' or 'false'))
-                    propnode.attrs[flg] = (v and 'true' or 'false')
+                    propertyNode.attrs[flg] = (v and 'true' or 'false')
                 end
             end
-            if (infos[cls][k].handleType or '') ~= '' then
-                propnode.attrs['handle-type'] = infos[cls][k].handleType
+            if (classInfo[className].properties[propertyName].handleType or '') ~= '' then
+                propertyNode.attrs['handle-type'] = classInfo[className].properties[propertyName].handleType
             end
-            if (infos[cls][k].label or '') ~= '' then
-                propnode.attrs.label = infos[cls][k].label
+            if (classInfo[className].properties[propertyName].label or '') ~= '' then
+                propertyNode.attrs.label = classInfo[className].properties[propertyName].label
             end
-            propnode.attrs.enum = infos[cls][k].enum
-            propnode.attrs['deprecated-by'] = infos[cls][k]['deprecated-by']
-            if (infos[cls][k].description or '') ~= '' then
-                table.insert(propnode.children, {tag = 'description', attrs = {}, children = {infos[cls][k].description}})
+            propertyNode.attrs.enum = classInfo[className].properties[propertyName].enum
+            propertyNode.attrs['deprecated-by'] = classInfo[className].properties[propertyName]['deprecated-by']
+            if (classInfo[className].properties[propertyName].description or '') ~= '' then
+                local descrNode = {
+                    tag = 'description',
+                    attrs = {},
+                    children = {classInfo[className].properties[propertyName].description},
+                }
+                table.insert(propertyNode.children, descrNode)
             end
-            table.insert(clsnode.children, propnode)
+            table.insert(classNode.children, propertyNode)
+        end
+
+        local namespacesNames = table.keys(classInfo[className].namespaces or {})
+        table.sort(namespacesNames)
+        for _, namespaceName in ipairs(namespacesNames) do
+            local nsinfo = classInfo[className].namespaces[namespaceName]
+            local namespaceNode = {
+                tag = 'namespace',
+                attrs = {
+                    name = namespaceName,
+                    ['new-property-forced-type'] = nsinfo.newPropertyForcedType,
+                },
+                children = {},
+            }
+            table.insert(classNode.children, namespaceNode)
         end
     end
-    output.xml = string.renderxml(output.xml, {'name', 'type', 'readable', 'writable', 'removable', 'silent'})
-    for ext, fileName in pairs(outputFile) do
-        local file = io.open(fileName, 'w')
-        if file then
-            file:write(output[ext])
-            file:close()
-        else
-            print('Error: Could not open output file ' .. fileName)
-        end
+    local attrOrder = {
+        'name',
+        'type',
+        'deprecated',
+        'readable',
+        'writable',
+        'removable',
+        'silent',
+        'constant',
+        'label',
+    }
+    local file = io.open(outputFileXML, 'w')
+    if file then
+        file:write(string.renderxml(classesNode, attrOrder))
+        file:close()
+    else
+        print('Error: Could not open output file ' .. outputFileXML)
     end
 
     sim.app:quit()
