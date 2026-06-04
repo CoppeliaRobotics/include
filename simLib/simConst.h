672089789373c3872f4db5b521e49ed49d3b5c14
Coppelia <mfreese@gmx.ch>
2026-06-04 16:54:41 +0200

    enum property

--- simLib/simConst.h
+++ simLib/simConst.h
@@ -657,6 +657,7 @@ enum { // property type
     sim_propertytype_handlearray,
     sim_propertytype_stringarray,
     sim_propertytype_group,
+    sim_propertytype_enum,
     sim_propertytype_method = 0xf0,
 };
 
